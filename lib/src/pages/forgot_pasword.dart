c8eeeb622fffd2743c0438a641f4ea677cb645e6
jcuellarv1966 <96638232+jcuellarv1966@users.noreply.github.com>
2024-02-19 11:14:32 -0500

    Finalize page forgot-password, and add Message Box ...

--- lib/src/pages/forgot_pasword.dart
+++ lib/src/pages/forgot_pasword.dart
@@ -1,5 +1,6 @@
 import 'package:curso_delivery_app/src/widgets/back_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ForgotPassword extends StatelessWidget {
   const ForgotPassword({super.key});
@@ -78,12 +79,90 @@ Widget _sendButton(BuildContext context) {
             shape: const BeveledRectangleBorder(
                 borderRadius: BorderRadius.all(Radius.circular(5))),
           ),
-          onPressed: () {},
+          onPressed: () {
+            _ShowAlerta(context);
+          },
           child: const Align(
             alignment: Alignment.center,
             child: Text("Send",
                 style: TextStyle(
                     fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center),
+          )));
+}
+
+// ignore: unused_element, non_constant_identifier_names
+void _ShowAlerta(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          // ignore: avoid_unnecessary_containers, sized_box_for_whitespace
+          content: Container(
+            height: 360,
+            child: Column(
+              children: [
+                const Image(
+                  image: AssetImage("assets/lock.png"),
+                  width: 130,
+                  height: 130,
+                ),
+                // ignore: avoid_unnecessary_containers
+                Container(
+                  margin: const EdgeInsets.all(15.0),
+                  child: Text(
+                    "Your password has been reset",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(15.0),
+                  child: Text(
+                    "You'll shortly receive an email with a code to setup a mew password.'",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+                _doneButton(context)
+              ],
+            ),
+          ),
+        );
+      });
+}
+
+// ignore: unused_element
+Widget _doneButton(BuildContext context) {
+  return Container(
+      width: 370.0,
+      height: 36.0,
+      margin: const EdgeInsets.only(top: 20.0),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.deepOrange,
+            shape: const BeveledRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(5))),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, "login");
+          },
+          child: const Align(
+            alignment: Alignment.center,
+            child: Text("Done",
+                style: TextStyle(
+                    fontSize: 17.0,
                     color: Colors.white,
                     fontWeight: FontWeight.w500),
                 textAlign: TextAlign.center),
